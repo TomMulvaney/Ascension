0c85ff08c5bfbeab7cb7f6fe39ce4eabbcca338a
TomMulvaney <mulvaney.tom@gmail.com>
2016-07-02 09:31:41 +0100

    Imported NGUI

---
+++ Ascension/Assets/Ascension/Behaviours/Movement/CharStates.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharStates : MonoBehaviour 
+{
+
+
+	enum States
+	{
+		Ground,
+		Air,
+		Wall,
+		Ceiling
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
